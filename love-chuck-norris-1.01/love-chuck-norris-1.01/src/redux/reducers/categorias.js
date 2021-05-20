bea81974c176f43d641506dbe37b202a9bcea147
pinkipurpurina <72076083+pinkipurpurina@users.noreply.github.com>
2021-05-20 02:32:09 +0200

    todo

--- love-chuck-norris-1.01/love-chuck-norris-1.01/src/redux/reducers/categorias.js
+++
@@ -1,8 +0,0 @@
-import { handleActions } from 'redux-actions';
-import { getCategoriasSuccess } from '../actions';
-
-export default handleActions({
-    [getCategoriasSuccess]: (state, action) => {
-        return action.payload;
-    },
-}, []);
